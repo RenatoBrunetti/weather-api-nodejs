b8fbbcd815a352c84eb0a4deb3a734c4adfe075e
Renato Brunetti <recarapia@gmail.com>
2019-09-03 20:00:40 -0300

    List and City

---
+++ api/models/cityinfo.js
@@ -0,0 +1,22 @@
+module.exports = function() {
+    
+    // Get city ID using city Name
+    this.getCityInfo = (cName, citiesList, info) => {
+        const city = citiesList.filter( (city) => {
+            const cityName = city.name.toLowerCase()
+            return cityName === cName
+        })
+
+        if(info === 'id'){
+            for(key in city){
+                if(city.hasOwnProperty(key)){
+                    const cityID = city[key].id
+                    return cityID
+                }
+            }
+        } else if(info === 'info'){
+            return city
+        }
+    }
+    return this
+}
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,7 +1,21 @@
 module.exports = function(application) {
 
     // Index Route
-    application.get('/', (req, res) => {
+    application.get('/', (req, res, next) => {
+        res.render('./')
+    })
+
+    const pagePath = (req, res) => {
+        const pageParam = req.params.page
+        const perPage = 10
+        const page = pageParam || 1
+        const pathPage = pageParam !== '' ? '../' : ''
+        return pathPage
+    }
+
+    const pageList = (req, res) => {
+        
+        const pathPage = pagePath(req, res)
 
         // Models - Get Cities => reader JSON file
         const cityListJson = 'data/city_list.json'
@@ -21,12 +35,52 @@ module.exports = function(application) {
         const citiesWithWeather = application.api.models.citiesweather()
         const citiesWeather = citiesWithWeather.getCityWeather(citiesList, weatherID)
 
-        // Render content
-        res.render('./', {
+        res.render('./list', {
+            pathpage: pathPage,
             cities: citiesList,
             weather: weathersList,
+            weatherid: weatherID,
             cityweather: citiesWeather
         })
+    }
 
+    // List
+    application.get('/list', (req, res, next) => {
+        pageList(req, res)
+    })
+    application.get('/list/:page', (req, res, next) => {
+        pageList(req, res)
+    })
+
+    // City Information
+    application.get('/city/:name/', (req, res) => {
+        
+        const pathPage = pagePath(req, res)
+
+        const cityName = req.params.name
+
+        // Models - Get Cities => reader JSON file
+        const cityListJson = 'data/city_list.json'
+        const citiesJson = application.api.models.reader()
+        const citiesList = citiesJson.getJson(cityListJson)
+
+        // Models - Get Weather => reader JSON file
+        const weatherListJson = 'data/weather_list.json'
+        const weathersJson = application.api.models.reader()
+        const weathersList = weathersJson.getJson(weatherListJson)
+
+        // Models - Get city ID using city Name
+        const cityFind = application.api.models.cityinfo(citiesList)
+        const cityID = cityFind.getCityInfo(cityName, citiesList, 'id')
+        const cityInfo = cityFind.getCityInfo(cityName, citiesList, 'info')
+
+        // Render content
+        res.render('./city', {
+            pathpage: pathPage,
+            cities: citiesList,
+            weather: weathersList,
+            cityid: cityID,
+            cityinfo: cityInfo
+        })
     })
 }
